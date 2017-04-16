75d43b7319fbc184c027de724bb76aa7a298ca07
theshanx <tiffany@puppetlabs.com>
2017-04-16 14:02:29 -0700

    Initial commit
    Before this commit, I missed Jeff and how he told us about all the
    baffling camps he had to pick his kid up from. Now I just miss Jeff.

---
+++ weiss_bot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+camp=$(/usr/local/bin/gshuf -n 1 ~/Desktop/kitten_code/weiss_bot/dangerous_activities.txt)
+
+echo 'time to take the kid to' $camp 
+say 'time to take the kid to ' $camp
